f4e627e329c82fc4c535e8afb74b5a6387abb2f6
Dale Sande <dale.sande@alaskaair.com>
2019-12-20 11:41:39 -0800

    chore: clean up resources
    The scope of this commit will;
    - remove dependency of running style dictionary locally
    - remove old unsed commit log template
    - update dependencies

--- scripts/styleDictionary.js
+++
@@ -1,5 +0,0 @@
-// Required dependency
-const StyleDictionary = require('style-dictionary').extend('./scripts/config.json');
-
-// Style Dictionary build function
-StyleDictionary.buildAllPlatforms();
